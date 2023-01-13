67fcf275682763a3f77065e138ad47714c800889
Eason iMac <eason@e-s.tw>
2023-01-14 02:31:16 +0800

    [feat] orbit smooth drag

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,35 @@
 <template>
     <div class="three">
-        <canvas ref="threeWrap" class="three__wrapper" />
-        <h1>THREE TEST</h1>
+        <canvas ref="threeWrap" class="three__wrapper"/>
+       <!-- <h1>THREE TEST</h1> -->
     </div>
 </template>
 <script setup>
-    import { nextTick, onMounted, ref } from 'vue'
+    import { nextTick, onMounted, ref } from 'vue';
     import * as THREE from 'three';
-    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+    import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+    import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
     
+   
     const threeWrap = ref()
     const objectLoadUrl = '/glbs/'
     const manager = new THREE.LoadingManager()
     const GLBLoader = new GLTFLoader(manager)
+    const RGBLoader = new RGBELoader()
+   
+
+// immediately use the texture for material creation
+    // const textureMaterial = new THREE.MeshBasicMaterial( { map: texture } );
+
 
     // 設定參數
     const fov = 20 // 鏡頭光學倍率
     const near = 0.1
     const far = 1000
-    const backgroundColor = new THREE.Color( 0x2DC4ED );
+    const backgroundColor = new THREE.Color( '#CFE2F4' );
     const light1_color = 0xffffff
-    const light2_color = 0x2DC4ED
+    const light2_color = 0xCFE2F4
 
     // 物件群組
     const heroGroup = new THREE.Group()
@@ -28,28 +37,45 @@
     // 材質設定 
     // 文件 https://threejs.org/docs/index.html?q=MeshPhysicalMaterial#api/en/materials/MeshPhysicalMaterial
     const material_1 = new THREE.MeshPhysicalMaterial({
-        color: 0xffffff,
-        metalness: 1,
-        roughness: 0.6,
-        transmission: 0.9,
-        // flatShading: true,
-        // transparent: true,
-        opacity: 1
+        color:  '#5288D8',
+        roughness: 0.5
+       
     })
     const material_2 = new THREE.MeshPhysicalMaterial({
         color: 0xffffff,
-        metalness: 1,
-        roughness: 0.6,
-        transmission: 0.9,
+        metalness: 0.1,
+        roughness: 0.3,
+        clearcoat:0.5,
+        clearcoatRoughness:0.5,
+        transmission: 1,
         // flatShading: true,
-        // transparent: true,
-        opacity: 1
+        transparent: true,
+        opacity: 1,
+        ior:1.3,
+        thickness : 1.5,
+        envMapIntensity:1,
+        specularIntensity:1,
+        specularColor:0xffffff
     })
-    const material_3 = new THREE.MeshMatcapMaterial({
+    const material_3 = new THREE.MeshPhysicalMaterial({
         color: 0xffffff,
+        metalness: 0.1,
+        roughness: 0.5,
+        clearcoat:0.5,
+        clearcoatRoughness:0.5,
+        transmission: 0.3,
+        // flatShading: true,
+        transparent: true,
+        opacity: 1,
+        ior:1.2,
+        thickness : 0.7,
+        envMapIntensity:1,
+        specularIntensity:1,
+        specularColor:0xffffff
+        
     })
 
-    let canvas, camera, renderer, scene;
+    let canvas, camera, renderer, scene, controls;
 
 
     const initThree = () => {
@@ -62,23 +88,36 @@
         camera.lookAt(0, 0, 0)
         camera.position.set(0,1,15)
 
+        RGBLoader.load('/kloppenheim_06_puresky_4k.hdr', (texture) => {
+            
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+            // scene.background = texture
+            scene.environment = texture
+        })
+
         // 燈光 (color, intensity)
         const light = new THREE.DirectionalLight(light1_color, 0.5)
-        const light2 = new THREE.DirectionalLight(light2_color, 2)
+        const light2 = new THREE.DirectionalLight(light2_color, 0.7)
         light.position.set(0, 5, 15)
-        light2.position.set(5, 0, -5)
+        light2.position.set(5, 0, 0)
         scene.add(light)
         scene.add(light2)
         
-        scene.background = backgroundColor
+        scene.background = backgroundColor;
         scene.add(heroGroup)
 
 
         renderer = new THREE.WebGLRenderer({ canvas })
         renderer.setSize(window.innerWidth, window.innerHeight)
         renderer.render(scene, camera)
+        renderer.physicallyCorrectLights = true;
         renderer.outputEncoding = THREE.sRGBEncoding;
 
+        controls = new OrbitControls( camera, renderer.domElement );
+        controls.enableDamping = true;
+        controls.dampingFactor = 0.05;
+        
+        controls.update();
         animate()
 
         console.log(scene)
@@ -119,9 +158,11 @@
         camera.aspect = window.innerWidth / window.innerHeight;
     }
     const animate = () => {
-        // console.log('test')
-        heroGroup.rotation.y += 0.01
+        controls.update();
+        // heroGroup.rotation.y += 0.01
+        // heroGroup.children[0].rotation.y += 0.05
         requestAnimationFrame( animate )
+        controls.update()
         renderer.render( scene, camera )
     }
 
