1485c32dfee3972d3dd6d49a789196836c455bfd
Eason iMac <eason@e-s.tw>
2023-01-14 20:49:57 +0800

    [feat] 晶瑩剔透的玻璃

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Cylinder from './components/Cylinder.vue'
 </script>
 
 <template>
@@ -10,11 +11,13 @@ import HelloWorld from './components/HelloWorld.vue'
         <a href="https://vuejs.org/" target="_blank">
         <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
         </a> -->
-        <HelloWorld />
+        <Cylinder />
+        <!-- <HelloWorld /> -->
+        <h1>THREE.JS RENDER TEST<br><span>PLAY FUN CYLINDER GLASS</span></h1>
     </div>
 </template>
 
-<style scoped>
+<style lang="scss">
 .logo {
 height: 6em;
 padding: 1.5em;
@@ -25,5 +28,19 @@ filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
 filter: drop-shadow(0 0 2em #42b883aa);
+}
+h1 {
+    position: fixed;
+    font-family: 'NT SEAWAVE (standart)';
+    top: 50%;
+    left: 50%;
+    z-index: 100;
+    text-align: center;
+    transform: translate(-50%, -50%);
+    line-height: 1;
+    
+    span {
+        font-size: 1rem;
+    }
 }
 </style>
